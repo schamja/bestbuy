9d10fd505decf780c09de78f160136045cb83aea
schamja Shahaga <schamja@gmx.de>
2026-01-13 15:22:49 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+Store Menu
+   ----------
+1. List all products in store
+2. Show total amount in store
+3. Make an order
+4. Quit
+Please choose a number:
+
+product_list = [ products.Product("MacBook Air M2", price=1450, quantity=100),
+                 products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 products.Product("Google Pixel 7", price=500, quantity=250)
+               ]
+best_buy = store.Store(product_list)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ store.py
@@ -0,0 +1,36 @@
+
+
+
+def add_product(self, product):
+    pass
+#bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+#mac = products.Product("MacBook Air M2", price=1450, quantity=100)
+
+# instance of a store
+#best_buy = Store([bose, mac])
+
+#pixel = products.Product("Google Pixel 7", price=500, quantity=250)
+#best_buy.add_product(pixel)
+
+
+def remove_product(self, product):
+    pass
+Entfernt ein Produkt aus dem Store.
+def get_total_quantity(self) -> int:
+    pass
+#Gibt zurück, wie viele Artikel insgesamt im Store vorhanden sind.
+def get_all_products(self) -> List[Product]:
+    pass
+#Gibt alle Produkte im Store zurück, die aktiv sind.
+def order(self, shopping_list) -> float:
+    pass
+#Erhält eine Liste von Tupeln, wobei jedes Tupel zwei Elemente enthält:
+#Produkt (Produktklasse) und Menge (int).
+#Kauft die Produkte und gibt den Gesamtpreis der Bestellung zurück.
+
+def get_total_quantity(self) -> int:
+    pass
+
+
+def get_total_quantity(self) -> int:
+    pass
